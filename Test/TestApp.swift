0457521157b77f54976a9f97cba4d9f0ec62809a
Bora Un <74696117+UnBora@users.noreply.github.com>
2024-02-07 21:33:44 +0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Un Bora on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
