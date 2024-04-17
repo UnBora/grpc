f2975243ae161deb86f0ca93a28435e96e41dfc5
Bora Un <74696117+UnBora@users.noreply.github.com>
2024-04-17 11:36:58 +0700

    test proto

---
+++ grpc-bo/proto/src/test/java/com/grpc/proto/ProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpc.proto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
